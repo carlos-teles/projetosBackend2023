0100e596c02d599a066fec6ed9c515ea2170d491
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-06-21 19:22:21 -0300

    Add files via upload

--- pythonProject106/app.py
+++ pythonProject106/app.py
@@ -12,14 +12,33 @@ def home():
 @app.route("/consulta-filme")
 def consulta_filme():
     import requests
-    response2 = requests.get("https://www.omdbapi.com/?apikey=XXXXXX&t=dune")
+    response2 = requests.get("https://www.omdbapi.com/?apikey=XXX&t=dune")
     return response2.json()
+
+@app.route("/consulta-filme-form", methods=["GET", "POST"])
+def consulta_filme_form():
+    if request.method == "POST":
+        import requests
+        response2 = requests.get("https://www.omdbapi.com/?apikey=XXX&t="+request.form["movie"])
+        return response2.json()
+    elif request.method == "GET":
+        return render_template("movie.html")
 
 @app.route("/consulta-list-productlines")
 def consulta_list_productlines():
     import requests
     response2 = requests.get("http://127.0.0.1:8000/list-productlines")
     return response2.json()
+
+@app.route("/consulta-list-productlines-form", methods=["GET", "POST"])
+def consulta_list_productlines_form():
+    if request.method == "POST":
+        import requests
+        response2 = requests.get("http://127.0.0.1:8000/get-productlines/"+request.form["product"])
+        return response2.json()
+    elif request.method == "GET":
+        return render_template("consulta_list_productlines_form.html")
+
 
 if __name__ == "__main__":
     app.run()
